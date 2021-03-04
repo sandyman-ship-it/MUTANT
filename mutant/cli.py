698bd9f1f5fce686dfd786a8aae92eddd5035510
sylvinite <i.sylvin@outlook.com>
2021-03-04 11:52:40 +0100

    Added more boiler

--- mutant/cli.py
+++ mutant/cli.py
@@ -8,3 +8,32 @@ import click
 import os
 import re
 import sys
+
+from mutant import version
+
+@click.group()
+@click.version_option(version)
+@click.pass_context
+def root(ctx):
+    """ Microbial Utility Toolbox And wrapper for data traNsmission and Transformation """
+    ctx.obj = {}
+
+@root.group()
+@click.pass_context
+def analyse(ctx):
+    pass
+
+@analyse.command()
+@click.pass_context
+def sarscov2(ctx):
+    pass
+
+@analyse.command()
+@click.pass_context
+def jasen(ctx):
+    pass
+
+@root.group()
+@click.pass_context
+def toolbox(ctx):
+    pass
