e25c079539412745f7a7b0d2cb178c1bb0bdf1c8
sylvinite <i.sylvin@outlook.com>
2021-03-18 11:32:36 +0100

    Installer now autoinstalls nextflow. Improved readme

---
+++ mutant/assets/nextflow.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -s https://get.nextflow.io | bash
+
+scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+if [[ ${PATH} != *"${scriptdir}"* ]]; then
+    export PATH=$PATH:$scriptdir
+fi
+
