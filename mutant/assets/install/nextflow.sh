aa2ff122be69491c9a39c783d2361ce803c378b6
Isak Sylvin <isak.sylvin@scilifelab.se>
2021-03-31 18:37:04 +0200

    halted nextflow auto download

--- mutant/assets/install/nextflow.sh
+++ mutant/assets/install/nextflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -s https://get.nextflow.io | bash
+# curl -s https://get.nextflow.io | bash
 
 scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [[ ${PATH} != *"${scriptdir}"* ]]; then
