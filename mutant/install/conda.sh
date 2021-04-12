1ea19a26870b42b5e05bfd8348431dace2c45bc5
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2021-04-12 10:16:48 +0200

    Fixed mutant installation, hasta compatible

--- mutant/install/conda.sh
+++ mutant/install/conda.sh
@@ -3,4 +3,22 @@
 SCRIPT=$(readlink -f $0)
 scriptdir=`dirname $SCRIPT`
 NAME=$1
-conda env create --name $NAME -f $scriptdir/environment.yaml
+
+echo "INFO - Unloading active environment"
+conda info| tac | tac | grep -q $NAME && source deactivate || :
+echo "INFO - Removing existing instance of environment (if present)"
+conda remove -y -n $NAME --all || :
+
+echo "INFO - Creating new environment"
+conda create --yes --name $NAME python=3.7
+source activate $NAME
+
+echo "INFO - Installing requirements"
+conda config --add channels bioconda
+pip install -r ${scriptdir}/../../requirements-pip.txt
+conda install --yes --file ${scriptdir}/../../requirements-conda.txt
+
+echo "INFO - Installing MUTANT"
+pip install -e .
+echo "INFO - Installation complete!"
+
--- setup.sh
+++ setup.sh
@@ -4,7 +4,3 @@ ENVNAME=$1
 
 bash ./mutant/install/nextflow.sh
 bash ./mutant/install/conda.sh $ENVNAME
-
-conda activate $ENVNAME
-pip install -r requirements.txt
-pip install -e .
